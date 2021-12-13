89ad44e9dd56e4c17cc8a70acf9672cecc5ff1ea
unknown <tlouet@gamingcampus.local>
2021-12-13 13:11:48 +0100

    menus + audio working with playerprefs

---
+++ TwitchGame/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField]
+    private AudioMixer _audioMixer;
+
+    public void Start()
+    {
+        SetAllVolumes();
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Application quit");
+    }
+
+    public void PlayGame()
+    {
+        //start game scenes
+    }
+
+    private void SetAllVolumes()
+    {
+        if (PlayerPrefs.HasKey("mainVolume"))
+        {
+            _audioMixer.SetFloat("mainVolume", PlayerPrefs.GetFloat("mainVolume"));
+        }
+        if (PlayerPrefs.HasKey("sfxVolume"))
+        {
+            _audioMixer.SetFloat("sfxVolume", PlayerPrefs.GetFloat("sfxVolume"));
+        }
+        if (PlayerPrefs.HasKey("musicVolume"))
+        {
+            _audioMixer.SetFloat("musicVolume", PlayerPrefs.GetFloat("musicVolume"));
+        }
+
+    }
+}
