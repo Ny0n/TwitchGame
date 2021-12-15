15325982f5f143a6a6d1a686d9f38a7223c00bb5
Ny0n <flav38320@gmail.com>
2021-12-15 20:54:45 +0100

    Finish the main menu

--- TwitchGame/Assets/Scripts/UI/GameSettingsMenu.cs
+++ TwitchGame/Assets/Scripts/UI/GameSettingsMenu.cs
@@ -1,19 +1,63 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameSettingsMenu : MonoBehaviour
 {
+    [SerializeField] private ScriptableSettings _settings;
     
-    // Start is called before the first frame update
-    void Start()
+    [Header("Max players")]
+    [SerializeField] private TMP_Text _maxPlayersText;
+    [SerializeField] private int _maxPlayersMin = 2;
+    [SerializeField] private int _maxPlayersMax = 50;
+    
+    [Header("Round duration")]
+    [SerializeField] private TMP_Text _roundDurationText;
+    [SerializeField] private float _roundDurationMin = 5;
+    [SerializeField] private float _roundDurationMax = 60;
+
+    private void Start()
     {
-        
+        RefreshUI();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void RefreshUI()
     {
+        OnMaxPlayersChanged(_settings.MaxNumberOfPlayers.ToString());
+        OnRoundDurationChanged(_settings.RoundDuration.ToString());
+    }
+
+    public void OnMaxPlayersChanged(string value)
+    {
+        if (int.TryParse(value, out int r)) // if we typed a correct int
+        {
+            if (r >= _maxPlayersMin && r <= _maxPlayersMax)
+            {
+                _settings.MaxNumberOfPlayers = r;
+                _maxPlayersText.text = r.ToString();
+            }
+        }
+    }
+    
+    public void OnRoundDurationChanged(string value)
+    {
+        if (float.TryParse(value, out float r)) // if we typed a correct float
+        {
+            if (r >= _roundDurationMin && r <= _roundDurationMax)
+            {
+                _settings.RoundDuration = r;
+                _roundDurationText.text = r.ToString();
+            }
+        }
+    }
+
+    public void LoadFromGoogle()
+    {
+        int maxPlayers = 20;
+        float roundDuration = 15f;
         
+        _settings.MaxNumberOfPlayers = maxPlayers;
+        _settings.RoundDuration = roundDuration;
+        
+        RefreshUI();
     }
 }
